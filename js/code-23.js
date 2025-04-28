4fc9633c5c80a7e1618f18bfd04517d62391aa64
z50205 <z50205@yahoo.com.tw>
2025-04-28 11:22:00 +0800

    User 23 first init commit.

---
+++ js/code-23.js
@@ -0,0 +1,10 @@
+let user_23_root=document.querySelector(".user-23");
+user_23_root.textContent="";
+let user_23_text=document.createElement("div");
+user_23_text.textContent="User Number 23:"
+user_23_text.className="user_23_text";
+let user_23_img=document.createElement("img");
+user_23_img.className="user_23_img";
+user_23_img.src="https://t1.pixhost.to/thumbs/5351/593054906_photo_2025-04-28_11-06-09.jpg";
+user_23_root.appendChild(user_23_text);
+user_23_root.appendChild(user_23_img);
